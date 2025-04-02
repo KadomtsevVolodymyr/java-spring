47f95db4a3f020a86f61556cf3b055712328eb24
KadomtsevVolodymyr <91778921+KadomtsevVolodymyr@users.noreply.github.com>
2025-04-02 15:29:30 +0200

    fix

--- src/test/java/com/volodymyrKadomtsev/medicalrecord/MedicalRecordApplicationTests.java
+++ src/test/java/com/volodymyrKadomtsev/medicalrecord/MedicalRecordApplicationTests.java
@@ -1,10 +1,10 @@
-package com.volodymyrKadomtsev.carsharing;
+package com.volodymyrKadomtsev.medicalrecord;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CarsharingApplicationTests {
+class MedicalRecordApplicationTests {
 
 	@Test
 	void contextLoads() {
